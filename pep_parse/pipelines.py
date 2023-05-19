66c405ffc5ed670310362d55eaf853c836d7bfea
Каролина Ефремова <efremova.karolina@gmail.com>
2023-05-19 20:22:59 +0700

    Третье ревью

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -4,7 +4,6 @@ from datetime import datetime as dt
 from pathlib import Path
 
 DATETIME_FORMAT = '%m-%d-%Y_%H-%M-%S'
-DATETIME = dt.now().strftime(DATETIME_FORMAT)
 
 BASE_DIR = Path(__file__).parent.parent
 DIR_RESULT = BASE_DIR / 'results'
@@ -21,7 +20,8 @@ class PepParsePipeline:
 
     def close_spider(self, spider):
         DIR_RESULT.mkdir(exist_ok=True)
-        file_status = BASE_DIR / 'results' / f'status_summary_{DATETIME}.csv'
+        time = dt.now().strftime(DATETIME_FORMAT)
+        file_status = BASE_DIR / 'results' / f'status_summary_{time}.csv'
         with open(file_status, mode='w', encoding='utf-8', newline='') as file:
             writer = csv.writer(file)
             writer.writerow(('Статус', 'Количество'))
